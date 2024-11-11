e2e8acb414d15de7305cc80225c8aa5f95c7066a
OmNai442003 <omnayinor99@gmail.com>
2024-11-11 13:40:47 -0800

    bug fix of landing

--- src/App.js
+++ src/App.js
@@ -6,47 +6,47 @@ import LandingPage from './screens/Landing/LandingPage';
 import UserProfile from './screens/UserProfile/UserProfile';
 import Task1Page_Control from './screens/Task1/Task1Page_Control';
 import Task1Page_Experiment from './screens/Task1/Task1Page_Experiment';
+import Login from './screens/Login/login';
 import Task2Page_Control from './screens/Task2/Task2Page_Control';
 import Task2Page_Experiment from './screens/Task2/Task2Page_Experiment';
 import Task4Page_Control from './screens/Task4/Task4Page_Control';
 import Task4Page_Experiment from './screens/Task4/Task4Page_Experiment';
-import Login from './screens/Login/Login';
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Login />
+    path:"/landing",
+    element: <LandingPage/>
   },
   {
-    path: "/landing",
-    element: <LandingPage />
+    path:"/userprofile",
+    element: <UserProfile/>
   },
   {
-    path: "/userprofile",
-    element: <UserProfile />
+    path:"/task1A",
+    element: <Task1Page_Control/>
   },
   {
-    path: "/task1A",
-    element: <Task1Page_Control />
+    path:"/task1B",
+    element: <Task1Page_Experiment/>
   },
   {
-    path: "/task1B",
-    element: <Task1Page_Experiment />
+    path:"/task2A",
+    element: <Task2Page_Control/>
   },
   {
-    path: "/task2A",
-    element: <Task2Page_Control />
+    path:"/task2B",
+    element: <Task2Page_Experiment/>
+  },{
+    path:"/task4A",
+    element: <Task4Page_Control/>
   },
   {
-    path: "/task2B",
-    element: <Task2Page_Experiment />
-  }, {
-    path: "/task4A",
-    element: <Task4Page_Control />
+    path:"/task4B",
+    element: <Task4Page_Experiment/>
   },
   {
-    path: "/task4B",
-    element: <Task4Page_Experiment />
+    path:"/",
+    element: <Login/>
   }
 ]);
 
